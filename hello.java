d8e136144c0c279a33e714f50a19082aa8f9c6ba
Toan H <Toan H@DESKTOP-2BAVN5L>
2023-08-27 16:36:36 -0500

    Initial Commit

--- hello.java
+++ hello.java
@@ -2,5 +2,7 @@ public class Hello {
 	public static void main (String args []) {
 		//print out my name.
 		System.out.println ("My name is Toan Huynh!");
+		//print out my major. 
+		System.out.println ("MY major is Computer Science!");
 	}
 }
